a170a29026f9a84e989344cdd670b5758c282f50
sven <sven@gwelt.net>
2020-02-23 23:19:12 +0100

    JSON and HTTPrequest

--- ESP32_epaper.ino
+++ ESP32_epaper.ino
@@ -370,20 +370,21 @@ void epaper_print_headline(String text)
 	display.updateWindow(0, 0, GxEPD_WIDTH, 100, true);
 }
 void epaper_print_content(const String& text) {epaper_update(0, 105, GxEPD_WIDTH, 120, text, f12, false);}
-void epaper_print_status1(const String& text) {epaper_update(0, GxEPD_HEIGHT-60, GxEPD_WIDTH, 30, text, f12, false);}
-void epaper_print_status2(const String& text) {epaper_update(0, GxEPD_HEIGHT-30, GxEPD_WIDTH, 30, text, f12, false);}
+void epaper_print_status1(const String& text) {epaper_update(0, GxEPD_HEIGHT-60, GxEPD_WIDTH, 30, text, f12, true);}
+void epaper_print_status2(const String& text) {epaper_update(0, GxEPD_HEIGHT-30, GxEPD_WIDTH, 30, text, f12, true);}
 
 void epaper_update(uint16_t x, uint16_t y, uint16_t w, uint16_t h, const String& text, const GFXfont* font, bool invert)
 {
 	display.updateWindow(0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, false);
-	/*
-	display.fillRect(x, y, w, h, GxEPD_BLACK);
-	display.updateWindow(x, y, w, h, true);
-	*/
-	display.fillRect(x, y, w, h, GxEPD_WHITE);
+	if (invert) {
+		display.fillRect(x, y, w, h, GxEPD_BLACK);
+		display.setTextColor(GxEPD_WHITE);		
+	} else {
+		display.fillRect(x, y, w, h, GxEPD_WHITE);
+		display.setTextColor(GxEPD_BLACK);
+	}
 	display.setCursor(x,y+22);
 	display.setFont(font);
-	display.setTextColor(GxEPD_BLACK);
 	display.println(text);
 	display.updateWindow(x, y, w, h, true);
 }
